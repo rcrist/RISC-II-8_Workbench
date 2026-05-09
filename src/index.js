e3115fd0d0e61804158050affda804c2d558a28f
rcrist <rickcrist99@gmail.com>
2026-05-09 12:59:49 -0600

    Duplication removal

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { createWorkbench, initializeWorkbench } from './app.js';
-
-const root = document.getElementById('app');
-root.innerHTML = createWorkbench();
-
-window.clock = initializeWorkbench(root);
